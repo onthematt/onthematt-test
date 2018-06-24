ce4e6beaaf920c23cb444d30558c2e3fa265c9b5
Matt <onthematt@hotmail.co.uk>
2018-06-24 23:25:45 +0100

    javaaaaa magikkk

--- onthematt.js
+++ onthematt.js
@@ -5,6 +5,11 @@ onthematt[2] = "this is test 2";
 onthematt[3] = "this is test 3";
 onthematt[4] = "this is test 4";
 onthematt[5] = "this is test 5";
+onthematt[6] = "this is test 6";
+onthematt[7] = "this is test 7";
+onthematt[8] = "this is test 8";
+onthematt[9] = "this is test 9";
+onthematt[10] = "this is test 0";
 
 function onTheMatt() {
 	var randomPost = Math.floor(Math.random()*(onthematt.length));
