85b746e7f998cd14a71125461e297e30604b09ea
Matt <onthematt@hotmail.co.uk>
2018-06-24 21:51:51 +0100

    random page!!!!

--- onthematt.js
+++ onthematt.js
@@ -1,10 +1,12 @@
-var blog = []
-blog [0] = "this is test 0";
-blog [1] = "this is test 1";
-blog [2] = "this is test 2";
+var onthematt = []
+onthematt[0] = "That's when you know you've found somebody special, when you can just shut the fuck up for a minute and comfortably enjoy the silence.";
+onthematt[1] = "'scuse me while I kiss the sky.";
+onthematt[2] = "this is test 2";
+onthematt[3] = "this is test 3";
+onthematt[4] = "this is test 4";
+onthematt[5] = "this is test 5";
 
-function blog () {
-	var randomPost = Math.floor(Math.random()*(blog.length));
-	document.getElementById('blogger').innerHTML = blog[randomPost];
-
+function onTheMatt() {
+	var randomPost = Math.floor(Math.random()*(onthematt.length));
+	document.getElementById('blogpost').innerHTML = onthematt[randomPost];
 }
